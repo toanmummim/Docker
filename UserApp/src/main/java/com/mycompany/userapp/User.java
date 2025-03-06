3cee7ae2d39ad8614779826b62752c4f949e1456
Toanmummim <toannguyen01216@gmail.com>
2025-03-06 22:40:58 +0700

    Userclass

---
+++ UserApp/src/main/java/com/mycompany/userapp/User.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.userapp;
+
+/**
+ *
+ * @author toan0
+ */
+public class User {
+    private String userId;
+    private String email;
+
+    public User(String userId, String email) {
+        this.userId = userId;
+        this.email = email;
+    }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public void updateEmail(String newEmail) { this.email = newEmail; }
+}
